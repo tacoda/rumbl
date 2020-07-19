fa23b97229ac620e122901597646610b1b4dfff1
Ian Johnson <tacoda@pm.me>
2020-07-19 14:31:19 -0400

    Add logging in and authentication plug

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Accounts do
 
 	alias Rumbl.Repo
 	alias Rumbl.Accounts.User
+	alias Pbkdf
 
 	def create_user(attrs \\ %{}) do
 		%User{}
@@ -20,6 +21,22 @@ defmodule Rumbl.Accounts do
 		%User{}
 		|> User.registration_changeset(attrs)
 		|> Repo.insert()
+	end
+
+	def authenticate_by_username_and_pass(username, given_pass) do
+		user = get_user_by(username: username)
+
+		cond do
+			user && Pbkdf2.verify_pass(given_pass, user.password_hash) ->
+				{:ok, user}
+
+			user ->
+				{:error, :unauthorized}
+
+			true ->
+				Pbkdf2.no_user_verify()
+				{:error, :not_found}
+		end
 	end
 
 	def change_user(%User{} = user) do
